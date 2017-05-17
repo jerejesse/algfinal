14e23003598eb10a286d203efda5f6055ee442d4
jesse <jerejesse@hotmail.com>
2017-05-17 22:15:06 +0800

    create master branch

---
+++ segmenttree.cpp
@@ -0,0 +1,45 @@
+
+
+/*x[1~n]
+array[m][2]
+
+
+*/
+
+node *root;
+
+class node{
+public:
+	int start;
+	int end;
+	int num;
+	int *arr;
+	node *left;
+	node *right;
+	node *parent;
+	node(){
+		num=0;
+		left=NULL;
+		right=NULL;
+	}
+private:
+	
+};
+
+void build(){
+	root= new node;
+	node *now;
+	now=root;
+	node *stacknode[1000];
+	
+	for(int i=0;i<n;i++){
+		now.left=new node;
+		now.right=new node;
+		now
+	}
+}
+
+void buildnode(node* now,
+
+
+
